385eedf4a3e333d7b2d6ff1bec4c6214c396b614
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-01-24 22:51:31 +0530

    Step 1.1 - Things to know in C++ added successfully.

--- Step 1 - Learn the basics/Step 1.1 - Things to Know in C++/Java/Python or any language/main.cpp
+++ Step 1 - Learn the basics/Step 1.1 - Things to Know in C++/Java/Python or any language/main.cpp
@@ -2,6 +2,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printName()
+{
+    cout << "Hello Raj" << endl;
+}
+
+void add(int a, int b)
+{
+    int sum = a + b;
+    cout << "The sum is : " << sum << endl;
+}
+
+int power(int a, int n)
+{
+    return pow(a, n);
+}
+
+int maxx(int a, int b)
+{
+    if (a >= b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+void doSomething(int n)
+{
+    cout << "The function started. " << n << endl;
+    n = n + 20;
+    cout << "The function ended : " << n << endl;
+}
+
+void doSomethingRef(int &n)
+{
+    cout << "The function started. " << n << endl;
+    n = n + 20;
+    cout << "The function ended : " << n << endl;
+}
 int main()
 {
 
@@ -252,6 +293,90 @@ int main()
             }
             cout << endl;
         }
+    */
+
+    // String
+    /*
+        string s = "Pravin";
+        int len = s.length();
+        cout<<"The length is : "<<len<<endl;
+        cout << s[2] << endl;
+    */
+
+    // For loop
+
+    /*
+        for (int i = 0; i <= 10; i++)
+        {
+            cout << "Hello" << endl;
+        }
+    */
+
+    // While loop
+
+    /*
+        int i = 1;
+        while(i<=5){
+            cout<<"Hello "<<i<<endl;
+            i++;
+        }
+    */
+
+    // do...while loop
+
+    /*
+        int i = 0;
+        do
+        {
+            cout << "Hello " << i << endl;
+            i++;
+        } while (i >= 10);
+
+        cout<<i<<endl;
+    */
+
+    // Function - Functions are the set of the code that will perform something for you
+    // Function are used to modularized code
+    // Functions are used to increase the readability
+    // Functions are used to write the same code multiples time
+
+    // printName();
+
+    // add(2,3);
+
+    // cout<<"The power of 2 raised to 3 is : "<<power(2,3)<<endl;
+    /*
+        int num1, num2;
+        cin >> num1 >> num2;
+
+        int maximum = maxx(num1, num2);
+        cout << "The maximum number between the " << num1 << " and " << num2 << " is : " << maximum << endl;
+
+    */
+
+    // pass by value and pass by reference
+
+    /*
+        int num = 100;
+        cout << "Before calling the function : " << num << endl;
+        doSomething(num);
+
+        cout << "After the function : " << num << endl;
+    */
+
+    // pass by reference
+
+    /*
+        int num = 100;
+        cout << "Before calling the function : " << num << endl;
+        doSomethingRef(num);
+
+        cout << "After the function : " << num << endl;
+
+    */
+
+    /*
+        Note : The arrays always pass by reference
     */
     return 0;
 }
